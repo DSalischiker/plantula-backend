f6da555be57beb90bdb238e62992c261ffb55fe4
Diego <diego@studiohakuna.com>
2022-06-15 16:27:17 -0300

    feat: plants route & controller

---
+++ src/controllers/plantController.js
@@ -0,0 +1,44 @@
+const createError = require("http-errors");
+var PlantModel = require("../models/plant");
+
+const limit = 20;
+
+exports.plants_find_paged = function (req, res, next) {
+  const plantId = req.query.plantId;
+  const page = req.query.page || 0;
+  const options = plantId
+    ? {
+        plant: plantId,
+        /* tableNumber: { $nin: [null, ""] },
+        voted: { $nin: [true] }, */
+      }
+    : {};
+  PlantModel.paginate(options, {
+    offset: page * limit,
+    limit,
+  })
+    .then((result) => {
+      return res.json(result);
+    })
+    .catch((error) => {
+      return next(error);
+    });
+};
+
+/* exports.plant_statistics = async function (req, res, next) {
+  if (!req.query.referrerId) return next(createError(400));
+  try {
+    const totals = await VoterModel.countDocuments({
+      referrer: req.query.referrerId,
+      tableNumber: { $nin: [null, ""] },
+    });
+    const totalsVoted = await VoterModel.countDocuments({
+      referrer: req.query.referrerId,
+      voted: true,
+      tableNumber: { $nin: [null, ""] },
+    });
+    return res.json({ totals, totalsVoted, referrerId: req.query.referrerId });
+  } catch (err) {
+    return next(createError(500));
+  }
+}; */
---
+++ src/routes/plant.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const passport = require("passport");
+const plantsController = require("../controllers/plantsController");
+
+const router = express.Router();
+
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  plantsController.plants_find_paged
+);
+
+/* router.get(
+  "/statistics",
+  passport.authenticate("jwt", { session: false }),
+  referralsController.referral_statistics
+); */
+
+module.exports = router;
